a5a79ef362d0aa4482dbaa23323fe134fa0e1641
Jakub Wieczorek <j.kuba.wieczorek1@gmail.com>
2025-01-04 13:15:19 +0100

    Lab2

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/repository/PatientRepository.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/repository/PatientRepository.java
@@ -2,7 +2,17 @@ package com.jpacourse.persistence.repository;
 
 import com.jpacourse.persistence.entity.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
 
+    @Query("SELECT p FROM PatientEntity p WHERE p.lastName = :lastName")
+    List<PatientEntity> findByLastName(@Param("lastName") String lastName);
+
+    @Query("SELECT p FROM PatientEntity p WHERE p.dateOfBirth = :dateOfBirth")
+    List<PatientEntity> findByDateOfBirth(@Param("dateOfBirth") LocalDate dateOfBirth);
 }
